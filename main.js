798989a02d864ec10126543b69de5cf66ffed339
AlexMato <matontialessandro0@gmail.com>
2020-04-16 20:44:58 +0200

    log di nomi e cognomi studenti

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  // INIZIO PRIMA PARTE
      // creo un oggetto con dati studente Luca
      var luca = {
        "nome":"Luca",
@@ -9,5 +10,37 @@ $(document).ready(function(){
      for (var dati in luca){
        console.log(dati + " : " +luca[dati]);
      }
-    
+  // FINE PRIMA PARTE
+  // INIZIO SECONDA PARTE
+  // creo elenco studenti
+     var studenti = [{
+       "nome" : "Carlo",
+       "cognome" : "Mazzetti",
+       "età" : 29
+     },
+     {
+       "nome" : "Francesca",
+       "cognome" : "Suppiniddi",
+       "età" : 38
+     },
+     {
+       "nome" : "Franco",
+       "cognome" : "Sgaramarreddu",
+       "età" : 32
+     }
+   ]
+   console.log(studenti);
+   // ciclo gli studenti
+    for (var i = 0; i < studenti.length; i++){
+      // salvo ogni studente
+      var eachStud = studenti[i];
+      // salvo il nome di ogni studente ciclato
+      var nameStud = eachStud.nome;
+      // salvo il cognome di ogni studente ciclato
+      var surnameStud = eachStud.cognome;
+      console.log(nameStud + " " + surnameStud);
+      
+    }
+  // FINE SECONDA PARTE
+
 });
